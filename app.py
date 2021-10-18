0ce6e40f6e21ffe82d583b9bc0a1cb0da84adca2
Zelhorst92 <r.zelhorst@gmail.com>
2021-10-18 12:48:19 +0000

    Fix no parameters show all

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def home():
 def search():
     query = request.form.get("query")
     query2 = request.form.get("query2")
-    if query == "" and query2 == None:
+    if query == "" and query2 == "":
         recipes = mongo.db.recipes.find().sort("recipe_name", 1)
         return render_template("recipes.html", recipes=recipes)
     elif query == "":
